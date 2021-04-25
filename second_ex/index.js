6c128a0e47525598cb39341dbc9c234b10aaab38
alsu <9.alsulu@gmail.com>
2021-04-25 23:42:37 +0300

    added homework

---
+++ second_ex/index.js
@@ -0,0 +1,23 @@
+function sum() {
+    let a = prompt('Введите первое число:');
+    let b = prompt('Введите второе число:');
+    alert(Number(a)+Number(b));
+}
+
+function sub() {
+    let a = prompt('Введите первое число:');
+    let b = prompt('Введите второе число:');
+    alert(a-b);
+}
+
+function mult() {
+    let a = prompt('Введите первое число:');
+    let b = prompt('Введите второе число:');
+    alert(a*b);
+}
+
+function div() {
+    let a = prompt('Введите первое число:');
+    let b = prompt('Введите второе число:');
+    alert(a/b);
+}
