fb504172645b30ac83a2e48e9b50587120423621
alsu <9.alsulu@gmail.com>
2021-10-03 23:09:58 +0300

    fixed

--- week_17/second_ex/index.js
+++ week_17/second_ex/index.js
@@ -1,16 +1,21 @@
 function add() {
     let comment1 = checkSpam();
-    document.getElementById("comments").innerHTML += `<p class="name">Аноним</p>`;
-    document.getElementById("comments").innerHTML += `<p class="comment">${comment1}</p>`;
+    if (comment1) {
+        document.getElementById("comments").innerHTML += `<p class="name">Аноним</p>`;
+        document.getElementById("comments").innerHTML += `<p class="comment">${comment1}</p>`;
+    }
 }
 
 function checkSpam() {
-    let added_comment = document.getElementById("text").value.toLowerCase();
-    let fixed_comment;
-    if (added_comment.indexOf("viagra") === -1 && added_comment.indexOf("xxx") === -1)
-    //или added_comment.includes("viagra")
-        fixed_comment = added_comment;
-    else
-        fixed_comment = added_comment.replace(/viagra|xxx/g, "***");
-    return fixed_comment;
+    let comment = document.getElementById("text").value
+    if (comment) {
+        let added_comment = comment.toLowerCase();
+        let fixed_comment;
+        if (added_comment.indexOf("viagra") === -1 && added_comment.indexOf("xxx") === -1)
+        //или added_comment.includes("viagra")
+            fixed_comment = added_comment;
+        else
+            fixed_comment = added_comment.replace(/viagra|xxx/g, "***");
+        return fixed_comment;
+    }
 }
--- week_18/third_ex/index.js
+++ week_18/third_ex/index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function(event) {
     let name = localStorage.getItem('name');
     let avatar = localStorage.getItem('avatar');
 
@@ -39,22 +39,27 @@ function add() {
     ctx.drawImage(image, 0, 0, image.width, image.height);
     let imgURL = canvas.toDataURL("image/png");
 
-    document.getElementById("comments").innerHTML += `<div class="one-comment"> <img src="${imgURL}"> <p class="name">${author}: </p> <p class="comment">${comment}</p> </div>`;
-
-    if (name !== author)
+    if (author && comment) {
+        if (name !== author)
         localStorage.setItem('name', author);
-        
-    if (!avatar || avatar !== imgURL)
-        localStorage.setItem('avatar', imgURL);
+    
+        if (!avatar || avatar !== imgURL)
+            localStorage.setItem('avatar', imgURL);
+    
+        document.getElementById("comments").innerHTML += `<div class="one-comment"> <img src="${localStorage.avatar}"> <p class="name">${localStorage.name}: </p> <p class="comment">${comment}</p> </div>`;
+    }
 }
 
 function checkSpam() {
-    let added_comment = document.getElementById("text").value.toLowerCase();
-    let fixed_comment;
-    if (added_comment.indexOf("viagra") === -1 && added_comment.indexOf("xxx") === -1)
-    //или added_comment.includes("viagra")
-        fixed_comment = added_comment;
-    else
-        fixed_comment = added_comment.replace(/viagra|xxx/g, "***");
-    return fixed_comment;
+    let comment = document.getElementById("text").value
+    if (comment) {
+        let added_comment = comment.toLowerCase();
+        let fixed_comment;
+        if (added_comment.indexOf("viagra") === -1 && added_comment.indexOf("xxx") === -1)
+        //или added_comment.includes("viagra")
+            fixed_comment = added_comment;
+        else
+            fixed_comment = added_comment.replace(/viagra|xxx/g, "***");
+        return fixed_comment;
+    } 
 }
