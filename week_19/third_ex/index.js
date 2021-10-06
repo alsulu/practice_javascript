9632482ee2a5f99cee94feba0a3d817ff02385e5
alsu <9.alsulu@gmail.com>
2021-10-06 08:49:58 +0300

    added ex3 of week19

---
+++ week_19/third_ex/index.js
@@ -0,0 +1,27 @@
+class Calc {
+    static value() {
+        this.numb1 = document.getElementById("first-number").value;
+        this.numb2 = document.getElementById("second-number").value;
+    }
+
+    static sum() {
+        this.value();
+        return Number(this.numb1) + Number(this.numb2);
+    }
+    static sub() {
+        this.value();
+        return this.numb1 - this.numb2;
+    }
+    static mul() {
+        this.value();
+        return this.numb1 * this.numb2;
+    }
+    static div() {
+        this.value();
+        return this.numb1 / this.numb2;
+    }
+}
+
+function result(result) {
+    document.getElementById("result").innerHTML = result;
+}
